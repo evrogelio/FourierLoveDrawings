8ed71b982bb35ccbe6e6fa581a308e0275e0d309
Rogelio Echavarria <rogelioev0812@gmail.com>
2025-02-12 21:57:03 -0600

    Landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
+	import { type EasingFunction, type TransitionConfig, fly, slide } from 'svelte/transition';
+	import { type LayoutProps } from './$types';
 	import '../app.css';
-	let { children } = $props();
+	import { cubicIn, expoOut, linear } from 'svelte/easing';
+	let { data }: LayoutProps = $props();
+
+	import { horizontalSlide } from '$lib/transitions';
 </script>
 
-{@render children()}
+{#key data.url}
+	<main class="grid h-screen w-screen items-center overflow-hidden bg-[#fff2ff] p-0">
+		<div class="h-full w-full">
+			<slot></slot>
+		</div>
+	</main>
+{/key}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,15 +2,29 @@
 	import { browser } from '$app/environment';
 	import { onMount } from 'svelte';
 
+	let canvasParent: HTMLDivElement;
 	onMount(async () => {
 		if (!browser) return;
 		const [{ default: p5 }, { default: startSketch }] = await Promise.all([
 			import('p5'),
 			import('$lib/landing-animation')
 		]);
-
-		const sketch = new p5(startSketch);
+		new p5(startSketch, canvasParent);
 	});
 </script>
 
-<main class="h-screen w-screen p-0 bg-black items-center justify-center grid"></main>
+<div class="flex h-full w-full items-center">
+	<div class="align-center flex h-full w-1/2 flex-col justify-center p-8">
+		<h1 class="borel pink text-8xl">Saint <br /> Valentines</h1>
+		<h2 class="dark-pink pl-4 text-3xl">Ilustrating love through Fourier epicycles</h2>
+		<div class="zain mt-8 flex w-full justify-end">
+			<a href="/epicycles"
+				><button
+					class="aspect-square w-16 items-center rounded-full bg-white text-center align-middle hover:bg-gray-100"
+					>Next</button
+				></a
+			>
+		</div>
+	</div>
+	<div class="h-full w-1/2" id="canvasParent" bind:this={canvasParent}></div>
+</div>
