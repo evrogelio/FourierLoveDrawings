e2279f9e3134b6954b40480a95f2af6a8249e22b
Rogelio Echavarria <rogelioev0812@gmail.com>
2025-02-14 14:59:57 -0600

    geek love

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@
 	<div class="align-center flex h-content lg:h-full lg:w-2/5 flex-col justify-center lg:p-8 p-4">
 		<h1 class="borel pink text-6xl/20 lg:text-8xl/32">Happy <br /> Valentines!</h1>
 		<h2 class="dark-pink pl-4 text-xl lg:text-3xl">
-			Love is just a perfect Fourier series—harmonics aligning in every heartbeat.
+			Geek love is just a perfect Fourier series—harmonics aligning in every heartbeat.
 		</h2>
 		<div class="zain w-full justify-end hidden lg:flex p-4">
 			<a href="/epicycles"
